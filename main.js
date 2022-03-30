574ae5d8b0473bb688d3c5ceb70ea6227d88b0e7
KrishnaM03 <96728162+KrishnaM03@users.noreply.github.com>
2022-03-30 17:08:58 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ function take_snapshot() {
 
 console.log('ml5 version', ml5.version);
 
-classifier = ml5.imageClassifier('model.json', modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/t3TByqqKS/model.json', modelLoaded);
 
 function modelLoaded(){
     console.log('Model Loaded');
